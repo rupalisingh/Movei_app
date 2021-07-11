118e6680f189e6799e11f9092962e49e6b15434e
Rupali Singh <rupalisingh2304@gmail.com>
2021-07-11 23:08:16 +0530

    Upto Delete and search

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -6,35 +6,87 @@ export default class Movies extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            movies: getMovies()
+            movies: getMovies(),
+            currSearchText: "",
+            //filterMovies: getMovies()
         }
     }
+
+    onDelete = (id) => {
+        let filterarr = this.state.movies.filter(movieObj => {
+            return movieObj._id != id
+        })
+
+        this.setState({
+            movies: filterarr
+        })
+    }
+    handleChange = (e) => {
+        let val = e.target.value
+        this.setState({
+            currSearchText: val
+        })
+        // if (val == '') {
+        //     this.setState({
+        //         filterMovies: this.state.movies,
+        //         currSearchText: ''
+        //     })
+        //     return
+        // }
+        // let filteredArr = this.state.movies.filter(movieObj => {
+        //     let title = movieObj.title.trim().toLowerCase()
+        //     return title.includes(val.toLowerCase())
+        // })
+
+        // this.setState({
+        //     filterMovies : filteredArr,
+        //     currSearchText : val
+        // })
+    }
     render() {
+        let { movies, currSearchText } = this.state
+        let filterMovies = []
+        if (currSearchText != '') {
+            filterMovies = this.state.movies.filter(movieObj => {
+                let title = movieObj.title.trim().toLowerCase()
+                return title.includes(currSearchText.toLowerCase())
+            })
+        } else {
+            filterMovies = movies
+        }
         return (
             <div className='row'>
                 <div className='col-3'>Hello World</div>
                 <div className='col-9'>
-                    <input type='text'></input>
-                    <table class="table table-dark table-hover">
+                    <input onChange={this.handleChange} type='text'></input>
+                    <table className="table table-dark table-hover">
                         <thead>
                             <tr>
                                 <th scope="col">Title</th>
                                 <th scope="col">Genre</th>
-                                <th scope="col">Stock</th>
-                                <th scope="col">Rate</th>
+                                <th scope="col">
+                                    Stock
+                                    <i className="fas fa-sort-up"></i>
+                                    <i className="fas fa-sort-down"></i>
+                                </th>
+                                <th scope="col">
+                                    Rating
+                                    <i className="fas fa-sort-up"></i>
+                                    <i className="fas fa-sort-down"></i>
+                                </th>
                                 <th></th>
 
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                this.state.movies.map(movieObj => (
-                                    <tr scope = 'row'>
+                                filterMovies.map(movieObj => (
+                                    <tr scope='row' key={movieObj._id}>
                                         <td>{movieObj.title}</td>
                                         <td>{movieObj.genre.name}</td>
                                         <td>{movieObj.numberInStock}</td>
                                         <td>{movieObj.dailyRentalRate}</td>
-                                        <td><button type="button" class="btn btn-danger">Delete</button></td>
+                                        <td><button onClick={() => this.onDelete(movieObj._id)} type="button" className="btn btn-danger">Delete</button></td>
 
                                     </tr>
                                 ))
